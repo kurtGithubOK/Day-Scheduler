d67b85def0e68fb3a83809a625625bb143957799
Kurt Heimerman <kurtheimerman@yahoo.com>
2021-09-01 21:46:09 -0700

    put data at top now rows display w/ existing tasks

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,28 +1,33 @@
 
 $(document).ready(() => {
+    // Data for app.  Gets saved to local storage when save is clicked.
+    data = {};
+
+    // Put time in header.
     updateHeader();
 
+    // Make list of hours to display.
     makeDisplay();
 
     function makeDisplay() {
-        // Get tasks from storage, later pass to makeDescriptionCol().
-        const tasks = getTasks();
-        let num = 8;
-        console.log('tasks:', tasks[num.toString()])
+        // // Get tasks from storage, later pass to makeDescriptionCol().
+        // const tasks = getTasks() || {};
+        // let num = 8;
+        // console.log('tasks:', tasks[num.toString()])
         
         const container = $('.container');
         // loop over hours and make hourrows.
         for(let hourIndex=0 ; hourIndex<24 ; hourIndex++) {
             // make a row.
-            const row = makeRow();            
+            const row = makeRow(hourIndex);            
 
             // fill in time column.
             const timeCol = makeTimeCol(hourIndex);
             timeCol.appendTo(row);
 
             // fill in description column.
-            console.log('tasks:', tasks[hourIndex.toString()])
-            const taskDescription = tasks[hourIndex.toString()];
+            console.log('data passed to cols:', data[hourIndex.toString()])
+            const taskDescription = data[hourIndex.toString()];
             const descriptionCol = makeDescriptionCol(hourIndex, taskDescription);
             descriptionCol.appendTo(row);
 
@@ -34,10 +39,11 @@ $(document).ready(() => {
     }
     
     // UI Makers ///////////////////////////////
-    function makeRow() {
+    function makeRow(hourIndex) {
         const row = $('<div>')
         row.addClass('row');
         // set color coding.
+        if()
         return row;
     }
     function makeTimeCol(hourIndex) {
@@ -55,13 +61,14 @@ $(document).ready(() => {
         textarea.appendTo(col);
         return col;
     }
-    function makeSaveCol(hourIndex) {
+    function makeSaveCol(hourIndex) { // renmae +lumn?
         const col = makeCol();
+        // Make button
         const saveButton = $('<button>');
         saveButton.attr('id', 'saveBtn'); // Needed?
         saveButton.addClass('saveBtn');
 
-        // Add icon
+        // Add icon to button
         const icon = $('<i data-hour-index=' + hourIndex + '>'); // improve this.
         icon.text('save me!');
         icon.appendTo(saveButton);
@@ -80,7 +87,8 @@ $(document).ready(() => {
         $('#currentDay').text(date)
     }
 
-    // Event handlers /////////////////////////////
+    // Event handler /////////////////////////////
+    // Save task description.
      $('i').click( function() {
         // figure out which one was clicked.
         const hourIndex = $(this).data('hourIndex');
@@ -90,29 +98,29 @@ $(document).ready(() => {
         const textareaSelector = 'textarea[id=' + hourIndex + ']';
         const enteredText = $(textareaSelector).val();
         // update task & save somehow?
-        updateTasks(hourIndex, enteredText);
         console.log('you entered:', enteredText)
+        updateTasks(hourIndex, enteredText);
     
 
     });
 
     // Utilities /////////////////////////////
-    function getTasks() {
-        const list = localStorage.getItem('dayScheduler');
-        return JSON.parse(list);
-    }
     function updateTasks(hoursIndex, taskDescription) {
-        const tasks = getTasks();
-        tasks[hoursIndex] = taskDescription;
-        localStorage.setItem('dayScheduler', JSON.stringify(tasks))
+        // const tasks = getTasks();
+        data[hoursIndex] = taskDescription;
+        localStorage.setItem('dayScheduler', JSON.stringify(data))
     }
+    
+    // function getTasks() {
+    //     const list = localStorage.getItem('dayScheduler');
+    //     return JSON.parse(list);
+    // }
+//     let data = {
+//         8: 'feed cat @ 9am',
+//         14: 'feed dog @ 3pm'
+//     }
 
-    let data = {
-        8: 'feed cat @ 9am',
-        14: 'feed dog @ 3pm'
-    }
-
-   localStorage.setItem('dayScheduler', JSON.stringify(data))
+//    localStorage.setItem('dayScheduler', JSON.stringify(data))
    // $('.description').click( (event) => {
     //     // Need to handle when user clicks twice in textarea???
     //     // change previously clicked tasks to divs, bt how???
